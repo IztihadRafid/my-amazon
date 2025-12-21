5fe3d20108bf4c404290062152d38d16ab6ce199
Iztihad Rafid <iztihadrafid10@gmail.com>
2025-12-21 19:56:11 +0600

    shadcn components added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    <Button>hello</Button>
-    </div>
-  );
+  return <Container className="bg-shop-light-pink">
+    <h2 className="text-xl font-semibold">Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam voluptatum, quam fugiat commodi officiis sit iusto voluptatibus blanditiis quas, tenetur sint alias veniam ex, fugit eaque maiores ad magni rem facere eius perferendis quia? Illum aliquid quis inventore quos? Corrupti optio ipsa deleniti aliquid veniam sed repudiandae architecto repellendus minus, ipsum corporis accusantium odit recusandae ut dolorum aliquam blanditiis sunt similique id odio ratione molestiae tenetur sapiente? Veniam neque voluptas quia voluptatum totam nisi amet impedit unde veritatis perspiciatis, tempora rerum saepe natus delectus dolorem accusamus commodi vero molestiae! Sit corporis temporibus quas! Fuga libero ut porro? Praesentium, expedita harum!</p>
+    <Button >Click me</Button>
+  </Container>;
 }
