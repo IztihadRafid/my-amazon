16411a9079d7dbfd7fae50907c72a8eb4a7222f4
Iztihad Rafid <iztihadrafid10@gmail.com>
2025-12-28 00:12:56 +0600

    notfound page added

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div className='flex items-center justify-center'>
+      <h1>Not Found 404</h1>
+    </div>
+  )
+}
+
+export default NotFoundPage
